08e234bb9d39c109e0e6616a14b3963853d544f2
ananthu <ask1234560@gmail.com>
2020-05-10 09:49:14 +0530

    updates

--- dotfiles/config/dmenu-4.9/config.def.h
+++ dotfiles/config/dmenu-4.9/config.def.h
@@ -5,18 +5,18 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"FiraCode Nerd Font:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeNorm] = { "#839496", "#002b36" },
+	[SchemeSel] = { "#002b36", "#2aa198" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
-static unsigned int lineheight = 0;         /* -h option; minimum height of a menu line     */
+static unsigned int lineheight = 26;         /* -h option; minimum height of a menu line     */
 
 /*
  * Characters not considered part of a word while deleting words
