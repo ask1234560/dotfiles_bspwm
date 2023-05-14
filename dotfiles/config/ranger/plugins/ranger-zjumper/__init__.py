4a29eec0e8887bf114713e5a9dfa32d1fdbf3afb
ananthu <ask1234560@gmail.com>
2023-05-14 19:22:20 +0530

    updates

--- dotfiles/config/ranger/plugins/ranger-zjumper/__init__.py
+++ dotfiles/config/ranger/plugins/ranger-zjumper/__init__.py
@@ -13,10 +13,9 @@ class z(Command):
         # location of rupa z source file
         z_loc = getenv("_Z_SRC")
         try:
-            arguments = 'source {} && _z -e {}'.format(
-                z_loc, " ".join(self.args[1:]))
-            cmd = ['zsh', '-c', arguments]
+            arguments = f'source {z_loc} && _z -e {" ".join(self.args[1:])}'
+            cmd = ['bash', '-c', arguments]
             directory = check_output(cmd).decode("utf-8").rstrip("\n")
             self.fm.execute_console("cd " + directory)
-        except Exception as e:
+        except Exception:
             raise Exception("Directory not found")
---
+++ dotfiles/config/scripts/python_upgrade_dependency.py
@@ -0,0 +1,34 @@
+import os
+import shlex
+import subprocess
+
+PATH = '/usr/lib/python3.10/site-packages'
+
+
+def main():
+    if not os.path.exists(PATH):
+        print(PATH, 'could not be found. No packages will need to be rebuilt.')
+        return
+    
+    command = ['pacman' , '-Qmq']
+    aur_list = subprocess.check_output(command, stderr=subprocess.PIPE, universal_newlines=True).split('\n')[:-1]
+
+    packages = set()
+    for name in os.listdir(PATH):
+        command = ['pacman', '-Qoq', os.path.join(PATH, name)]
+        try:
+            output = subprocess.check_output(command, stderr=subprocess.PIPE, universal_newlines=True)
+        except subprocess.CalledProcessError as exception:
+            print(f'Failed to execute "{" ".join(shlex.quote(part) for part in command)}": {exception.stderr.strip()}')
+            continue
+
+        # a file or folder might be owned by many packages, for example __pycache__
+        pkg = output.strip().split('\n')
+        if pkg[0] in aur_list:
+            packages.update(pkg)
+
+    for pkg in sorted(packages):
+        print(pkg)
+
+if __name__ == '__main__':
+    main()
