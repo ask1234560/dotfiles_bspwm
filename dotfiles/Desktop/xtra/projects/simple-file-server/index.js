cbf4f542db69efb78397f07e3aa3fa3f56dd1788
ananthu <ask1234560@gmail.com>
2024-06-16 12:33:17 +0530

    updates

---
+++ dotfiles/Desktop/xtra/projects/simple-file-server/index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const multer = require('multer');
+const path = require('path');
+
+// Set up storage for uploaded files
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/');
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + '-' + file.originalname);
+  }
+});
+
+// Create the multer instance
+const upload = multer({ storage: storage });
+
+app.post('/up', upload.single('file'), (req, res) => {
+  // Handle the uploaded file
+  res.json({ message: 'File uploaded successfully!' });
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/index.html'))
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
