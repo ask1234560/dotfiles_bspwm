27869503db0c9a676f3ca937251040e84a1881aa
ananthu <ask1234560@gmail.com>
2025-01-18 15:52:25 +0100

    updates

--- dotfiles/Desktop/xtra/projects/simple-file-server/index.js
+++ dotfiles/Desktop/xtra/projects/simple-file-server/index.js
@@ -1,31 +1,48 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const multer = require('multer');
-const path = require('path');
+const express = require("express");
+const app = express();
+const port = 3000;
+const multer = require("multer");
+const path = require("path");
 
 // Set up storage for uploaded files
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/');
+    cb(null, "uploads/");
   },
   filename: (req, file, cb) => {
-    cb(null, Date.now() + '-' + file.originalname);
-  }
+    console.log(`uploading file ${file}`);
+    cb(null, Date.now() + "-" + file.originalname);
+  },
 });
+
+function progress_middleware(req, res, next) {
+  let progress = 0;
+  const file_size = req.headers["content-length"];
+
+  // set event listener
+  req.on("data", (chunk) => {
+    progress += chunk.length;
+    const percentage = (progress / file_size) * 100;
+    console.log(`file progress ${percentage}`);
+  });
+
+  // invoke next middleware
+  next();
+}
 
 // Create the multer instance
 const upload = multer({ storage: storage });
 
-app.post('/up', upload.array('file'), (req, res) => {
+app.post("/up", progress_middleware, upload.array("file"), (req, res) => {
   // Handle the uploaded file
-  res.json({ message: 'File uploaded successfully!' });
+  res.json({ message: "File uploaded successfully!" });
 });
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/index.html'))
-})
+app.get("/", (req, res) => {
+  console.log(`request received from ip ${req.ip}`);
+  res.sendFile(path.join(__dirname, "/index.html"));
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Upload app listening on port ${port}`);
+});
