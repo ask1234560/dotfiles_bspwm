4542ae7d086825d3df5d05699d087ba3a6395509
ananthu <ask1234560@gmail.com>
2025-06-01 15:40:44 +0200

    eza fixes

---
+++ dotfiles/config/ranger/plugins/ranger_devicons/locales/fr.py
@@ -0,0 +1,10 @@
+translations = {
+    'Bureau': 'Desktop',
+    'Documents': 'Documents',
+    'Images': 'Pictures',
+    'Musique': 'Music',
+    'Publique': 'Public',
+    'Téléchargements': 'Downloads',
+    'Modèles': 'Templates',
+    'Vidéos': 'Videos',
+}
